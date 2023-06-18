cdfeff6d25f18c461813d6500bc4add13585e8fa
jurilents <keeeneeek@gmail.com>
2023-06-19 00:57:45 +0300

    Prepare to publish

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: 'https://api.voicer-demo.tacles.net', //process.env.REACT_APP_API_BASE_URL,
 });
 
 export const languagesApi = {
